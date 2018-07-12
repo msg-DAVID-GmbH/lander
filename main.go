610e74fb8c8f092d9978906e88bee83c014ce17d
David Daehne <david.daehne@msg-david.de>
2018-07-12 09:10:05 +0200

    - add struct for Container for lander to process - add function to get data from docker daemon - check in RenderAndRespond if the path is valid - add log messages on RenderAndResond

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"io"
 	/*"os"*/
+	"github.com/fsouza/go-dockerclient"
 	"log"
 	"net/http"
 )
@@ -13,13 +14,57 @@ type Config struct {
 	port    string
 }
 
+type Container struct {
+	appName  string
+	appURL   string
+	appGroup string
+}
+
+func getRoutes() []Container {
+	// set endpoint of docker daemon api
+	endpoint := "unix:///var/run/docker.sock"
+
+	// get new client
+	client, err := docker.NewClient(endpoint)
+	if err != nil {
+		panic(err)
+	}
+
+	// initialise containers struct slice
+	var landerRoutes []Container
+
+	// get running containers
+	containers, err := client.ListContainers(docker.ListContainersOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+	// iterate through slice of containers and find "lander" labels
+	for _, container := range containers {
+		if _, found := container.Labels["lander.enable"]; found {
+			log.Println("found lander labels on Container:", container.ID)
+			landerRoutes = append(landerRoutes, Container{appName: container.Labels["lander.name"], appURL: container.Labels["traefik.frontend.rule"], appGroup: container.Labels["lander.group"]})
+		}
+	}
+	return landerRoutes
+}
+
 func RenderAndRespond(w http.ResponseWriter, r *http.Request) {
-	/*conf := getConfig()*/
+	// check if the request is exactly "/", otherwise stop the response
+	if r.URL.String() != "/" {
+		log.Println(r.RemoteAddr, r.URL, "not a valid request")
+		return
+	}
+
+	// print request to log
 	log.Println(r.RemoteAddr, r.Method, r.URL)
+	// call getRoutes
+	routes := getRoutes()
+	log.Println(routes)
+	// answer the request
 	io.WriteString(w, "Hallo Welt!")
 }
 
-func getConfig() Config {
+func GetConfig() Config {
 	// get command line arguments
 	// commandLineArgs := os.Args[1:]
 
@@ -42,7 +87,7 @@ func getConfig() Config {
 
 func main() {
 	// get configuration
-	config := getConfig()
+	config := GetConfig()
 
 	// register handle function for root context
 	http.HandleFunc("/", RenderAndRespond)
